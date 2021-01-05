905c9b7bac0468926d0f9d63d82b22278705f2bc
Maksim Popov <ma.popov.ks@yandex.ru>
2021-01-05 04:34:21 +0300

    Added a pop-up window when you click on the "Log in" button. Implemented login based on registration data using firebase authentication.
    + Created a JSON Google file to work with firebase
    + In build.gradle updated the firebase library from 19.5.1 to 19.6.0
    + + In MainActivity.java replaced user. get Email with firebase authorization
    + + Created a file like sign_in_window registration for login (styles, buttons, implementation are registered)
    
    Added work with OSM maps (Zoom in, Zoom out, Maximum zoom in/Zoom out)
    
    + In build.gradle added library for working with the OSM Droid map
    + B AndroidManifest.xml added the "activity" line to navigate between windows (to go to MapActivity)
    
    - In MainActivity.java did not convert expressions to lambda (for a better understanding of the code)
    - In MainActivity.java imported the necessary libraries

--- app/src/main/java/diplom/application/run_around_world/MainActivity.java
+++ app/src/main/java/diplom/application/run_around_world/MainActivity.java
@@ -1,9 +1,11 @@
 package diplom.application.run_around_world;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -11,6 +13,7 @@ import android.view.View;
 import android.widget.Button; //подключилас библиотека для связывания кнопок
 import android.widget.RelativeLayout;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
@@ -51,8 +54,78 @@ public class MainActivity extends AppCompatActivity {
                 //Этот код будет выполняться при нажатии на кнопку регистрации
                 ShowRegisterWindow(); //вызываем функцию, которая реализована ниже
             }
-            
+
         });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() { //Установили для кнопки вход обработчик событий
+            @Override
+            public void onClick(View v) {
+                //Этот код будет выполняться при нажатии на кнопку вход
+                ShowSignInWindow(); //вызываем функцию, которая реализована ниже
+            }
+
+        });
+    }
+
+    private void ShowSignInWindow(){ //Функция, которая будет вызвана при нажатии на кнопку Вход
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this); //встроенный класс для отображения всплывающих окон и создали обхект dialog
+        dialog.setTitle("Войти"); //Указываем заголовок для всплывающего окна
+        dialog.setMessage("Введите данные для входа"); //Указываем подпись под заголовком
+
+        LayoutInflater inflater = LayoutInflater.from(this); //для получения шаблона, который создали ранее, чтобы в всплывающее окно поместить. Создаём класс In Falter
+        View sign_in_window = inflater.inflate(R.layout.sign_in_window, null); //создали переменную на основе класса View и помещаем наш ранее созданный шаблон
+        dialog.setView(sign_in_window); // устанавливаем шаблон для всплывающего окна
+
+        //Получаем данные текстовых полей в нашем шаблоне layout register_window
+        MaterialEditText email = sign_in_window.findViewById(R.id.emailField); // создаём объект email на основе класса MaterialEditText
+        MaterialEditText pass = sign_in_window.findViewById(R.id.passField); // создаём объект pass на основе класса MaterialEditText
+
+        //Создаём кнопки Отменить(Назад) и кнопку Отправить(и зарегестрировать пользователя в БД)
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() { // функция setNegativeButton для установления кнопки Отмена. Первый параметр отображаемый текст. Второй парамер - функция при нажатии на кнопку
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss(); //Скрывает при нажатии
+            }
+        });
+
+        dialog.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) { //названия параметров поменяли чтобы не повторялись
+                if (TextUtils.isEmpty(email.getText().toString())) { //Класс TextUtils даёт доступ к функции isEmpty, проверка на пустое поле
+                    Snackbar.make(root, "Введите вашу почту", Snackbar.LENGTH_SHORT).show(); //обращаемся к классу Snackbar для вызова всплывающего окна. LENGTH_SHORT - отвечает за длительность = 3 сек
+                    return;
+                }
+
+                if (pass.getText().toString().length() < 5) { //Класс TextUtils даёт доступ к функции isEmpty, проверка на пустое поле
+                    Snackbar.make(root, "Введите пароль, который более 5 символов", Snackbar.LENGTH_SHORT).show(); //обращаемся к классу Snackbar для вызова всплывающего окна. LENGTH_SHORT - отвечает за длительность = 3 сек
+                    return;
+                }
+
+                //Авторизация по Email и паролю
+                auth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                        //Вызываем обработчик события
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) { //В случае успешной авторизации вызывается onSuccess
+                               //Перебрасываем пользователя на новую страницу Activity
+                                startActivity(new Intent(MainActivity.this, MapActivity.class));
+                                finish();
+                            }
+                            //В случае неуспещной авторизации
+                        }).addOnFailureListener(new OnFailureListener() { //В случае неуспешной авторизации
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //Вызываем высплывающее окно при неуспешной авторизации
+                        Snackbar.make(root,"Ошибка авторизации" + e.getMessage(),Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        });
+
+        dialog.show();
+
     }
 
     private void ShowRegisterWindow() { //Добавили метод из контекстного меню после создания функции ShowRegisterWindow
@@ -71,7 +144,8 @@ public class MainActivity extends AppCompatActivity {
         MaterialEditText phone = register_window.findViewById(R.id.phoneField); // создаём объект phone на основе класса MaterialEditText
 
         //Создаём кнопки Отменить(Назад) и кнопку Отправить(и зарегестрировать пользователя в БД)
-        dialog.setNegativeButton("Отмениь", new DialogInterface.OnClickListener() { // функция setNegativeButton для установления кнопки Отмена. Первый параметр отображаемый текст. Второй парамер - функция при нажатии на кнопку            @Override
+        dialog.setNegativeButton("Отмениь", new DialogInterface.OnClickListener() { // функция setNegativeButton для установления кнопки Отмена. Первый параметр отображаемый текст. Второй парамер - функция при нажатии на кнопку
+            @Override
             public void onClick(DialogInterface dialogInterface, int which) {
                 dialogInterface.dismiss(); //Скрывает при нажатии
             }
@@ -112,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
                               user.setPhone(phone.getText().toString());
 
                               //Передаём наш объект в базу данных
-                              users.child(user.getEmail()) //Используем переменную users и обращаемся к функции child, которая идентифицирует пользователя по email (ключ пользователя)
+                              users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()) //Используем переменную users и обращаемся к функции child, которая идентифицирует пользователя по email (ключ пользователя)
                                       .setValue(user) // В таблицу user добавили нашего нового пользователя
 
                                       //Обработчик событий вызовется в слуае успешного добавления пользователя
